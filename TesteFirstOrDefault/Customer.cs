2e9d903e6c540b0a8ab9e4c161627688e03c3200
guilhermemap <guilherme@acheipneus.com.br>
2024-06-26 16:57:50 -0300

    Adicionar arquivos de projeto.

---
+++ TesteFirstOrDefault/Customer.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
---
+++ TesteFirstOrDefault/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<Customer> list = [
+    new Customer() {    Id = 1,    Name = "First",    Email="First@gmail.com"},
+    new Customer() {    Id = 2,    Name = "Second",    Email="Second@gmail.com"},
+    new Customer() {    Id = 3,    Name = "Third",    Email="Third@gmail.com"},
+];
+List<int> intList = [1, 2, 3, 4];
+
+var firstCustomer = list.FirstOrDefault(c => c.Id == 5); // null
+var firstInt = intList.FirstOrDefault(i => i == 6); // 0
+
+
+try
+{
+    var oops = list.First(c => c.Id == 5);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("first(): " + ex.Message);
+}
